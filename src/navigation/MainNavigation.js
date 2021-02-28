b91e70b6a0e8bd142419c54f3fa50dbf71161a0c
mori30r <ghahreman00@gmail.com>
2021-02-28 20:23:45 +0330

    Expo Boilerplate

---
+++ src/navigation/MainNavigation.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import MyPlantsScreen from "../screens/MyPlantsScreen";
+
+const Stack = createStackNavigator();
+
+function MainNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="plants" component={MyPlantsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainNavigation;
---
+++ src/screens/MyPlantsScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Text, View, StyleSheet } from "react-native";
+
+const MyPlantsScreen = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default MyPlantsScreen;
