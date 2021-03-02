ba37d9e7e0d8839d1dd501f1427524a4322a499b
mori30r <ghahreman00@gmail.com>
2021-03-02 18:01:43 +0330

    CameraScreen Added

---
+++ src/screens/CameraScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Text, View } from "react-native";
+
+const CameraScreen = () => {
+  return (
+    <View>
+      <Text>Camera Screen</Text>
+    </View>
+  );
+};
+
+export default CameraScreen;
