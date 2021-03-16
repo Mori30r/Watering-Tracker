27bf7d93fa0614152cf7fcadaa8e764c682b0efe
mori30r <ghahreman00@gmail.com>
2021-03-16 13:37:27 +0330

    Camera Added

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,13 +1,38 @@
-import React from "react";
-
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Camera } from "expo-camera";
+import { Text, View, StyleSheet, Alert } from "react-native";
 
 const CameraScreen = () => {
-  return (
-    <View>
-      <Text>Camera Screen</Text>
+  const [canStartCamera, setCanStartCamera] = useState(null);
+  const startCamera = async () => {
+    const { status } = await Camera.requestPermissionsAsync();
+    if (status === "granted") {
+      setCanStartCamera(true);
+    } else {
+      setCanStartCamera(false);
+      Alert.alert("Access Denied");
+    }
+  };
+
+  useEffect(() => {
+    startCamera();
+  }, []);
+
+  let camera;
+  return canStartCamera ? (
+    <Camera ref={(r) => (camera = r)} style={styles.camera}></Camera>
+  ) : (
+    <View style={styles.container}>
+      <Text>Access Denied</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {},
+  camera: {
+    flex: 1,
+  },
+});
 
 export default CameraScreen;
